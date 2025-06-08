2f5bd5b4c6b0bea30b92cd3cd6364d7aa57d6333
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-06-08 00:42:37 -0400

    created files

---
+++ dsPIC_deployment/MPLAB_X_Project/src/main.c
@@ -0,0 +1,10 @@
+/*
+
+This code is for the dspic microcontroller family.
+It performs isolated word recognition for capturing umms, uhhs, and duhs
+to help you improve your speaking habits.
+
+v0.0.1
+
+
+*/
